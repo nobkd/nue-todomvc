626ad3c3d958159a87917637b620356455e6f260
nobkd <44443899+nobkd@users.noreply.github.com>
2025-03-03 03:37:05 +0100

    fixes

--- src/model.js
+++ src/model.js
@@ -1,18 +1,17 @@
-import { router } from '/@nue/app-router.js'
 import { $ } from '/@nue/view-transitions.js'
 
 
-router.configure({ route: '/:filter/', persistent_params: ['todos'] })
-
-
-export function addTodo(e) {
-  const todo = Object.fromEntries(new FormData(e.target)).todo
-  console.log('addTodo:', todo)
-  e.target.reset()
+export function addTodo({ target }) {
+  console.log('addTodo:', target.value)
+  target.value = ''
 }
 
 export function removeTodo(e) {
   console.log('removeTodo:', e)
+}
+
+export function toggleAll(e) {
+  console.log('toggleAll:', e)
 }
 
 export function todoState(e) {
